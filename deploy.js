3bf198cc2bddbe01c0f5240a1e9914497f2cee99
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-04-21 16:37:23 -0400

    Changed deploy script

--- deploy.js
+++ deploy.js
@@ -2,4 +2,6 @@ const ghPages = require("gh-pages");
 
 ghPages.publish("build", {
   message: "Deploying app...",
-})
+  repo:  `https://${process.env.ACCESS_TOKEN}@github.com/OrangeDrangon/todo-react.git`,
+  silent: true
+});
